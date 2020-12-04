dde6336b66f7a03aa61895c43f25e5b9dc4e7166
Sanidhya Kumar <sanidhya09@gmail.com>
2020-12-04 17:08:59 +0530

    initial push

--- app/src/main/java/com/sandy/advancekotlinbase/di/NetworkModule.kt
+++ app/src/main/java/com/sandy/advancekotlinbase/di/NetworkModule.kt
@@ -4,10 +4,12 @@ import com.sandy.advancekotlinbase.network.NewsApi
 import com.sandy.advancekotlinbase.utility.BASE_URL
 import dagger.Module
 import dagger.Provides
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 @Module
@@ -20,7 +22,16 @@ object NetworkModule {
     internal fun provideRetrofitInterface(): Retrofit {
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = HttpLoggingInterceptor.Level.BODY
-        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+        val client = OkHttpClient.Builder().addInterceptor(interceptor)
+            .addInterceptor(Interceptor { chain ->
+                val builder = chain.request().newBuilder()
+                builder.header("Token", "TempAndroid")
+                return@Interceptor chain.proceed(builder.build())
+            })
+            .connectTimeout(30000, TimeUnit.SECONDS)
+            .readTimeout(2000, TimeUnit.SECONDS)
+            .build()
 
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
