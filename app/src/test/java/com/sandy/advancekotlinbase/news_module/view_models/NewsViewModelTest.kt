d499d93cd9a9b884dccdb90c01f577a8785cd003
Sanidhya Kumar <sanidhya09@gmail.com>
2021-03-17 13:31:50 +0530

    initial push test case Mockito

---
+++ app/src/test/java/com/sandy/advancekotlinbase/news_module/view_models/NewsViewModelTest.kt
@@ -0,0 +1,41 @@
+package com.sandy.advancekotlinbase.news_module.view_models
+
+import com.sandy.advancekotlinbase.news_module.models.request_models.NewsRequestModel
+import com.sandy.advancekotlinbase.news_module.network.NewsApiService
+import com.sandy.advancekotlinbase.news_module.network.NewsRemoteDataRepository
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+class NewsViewModelTest {
+    lateinit var viewModel: NewsViewModel
+
+    @Mock
+    lateinit var newsApiService: NewsApiService
+    lateinit var newsRemoteDataRepository: NewsRemoteDataRepository
+
+    private lateinit var newsRequestModel: NewsRequestModel
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        newsRequestModel = NewsRequestModel("in", "sports", "")
+        newsRemoteDataRepository = NewsRemoteDataRepository(newsApiService)
+        viewModel = NewsViewModel(newsRemoteDataRepository)
+
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun testOnRetrieveError() {
+        val s = "newsRequestModel"
+        viewModel.onRetrieveError(s)
+        Mockito.verify(viewModel)?.onRetrieveError(s)
+    }
+}
